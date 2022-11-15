49305198846ff067f30ca026e73828c6ed1ecf25
Jeffrey Dunaway <jeffreydunaway@users.noreply.github.com>
2022-11-14 20:07:41 -0800

    Update cardsWorthTen.js

--- cardsWorthTen.js
+++ cardsWorthTen.js
@@ -21,7 +21,11 @@ const cards = [
  * @param {array} cards
  * @return {string} displayVal
  */
-const cardsWorthTen = cards => {};
+const cardsWorthTen = cards => {
+ return cards.filter(card => card.val === 10).map(cards => cards.displayVal).join(',');
+};
+
+console.log(cardsWorthTen(cards));
 
 console.log(cardsWorthTen(cards));
 // should return/log "10, Jack, Queen, King"
