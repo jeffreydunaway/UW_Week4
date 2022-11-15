87e56e7a8acb59fcae877ff4fde3e8c7766b1254
Jeffrey Dunaway <jeffreydunaway@users.noreply.github.com>
2022-11-14 20:08:58 -0800

    Update createCardDeck.js

--- createCardDeck.js
+++ createCardDeck.js
@@ -5,8 +5,73 @@
 const getDeck = () => {
 
 }
+const cardSuit = ["hearts", "diamonds", "spades", "clubs"];
+  const faceCards = ["jack", "queen", "king", "ace"];
+  const  completeDeck = [];
 
+    for (let i = 0; i < cardSuit.length; i++) {
+        let suit = cardSuit[i];
 
+        let numberOfCardsInSuit = 1;
+        let cardNumber = 2;
+        let faceCardNumber = 0;
+
+          while (numberOfCardsInSuit < 10) {
+
+            if (numberOfCardsInSuit === 10) {
+              continue;
+            }
+
+              let deck = {
+                val: cardNumber,
+                displayVal: cardNumber.toString(),
+                suit: suit
+              }
+
+          completeDeck.push(deck);
+
+          cardNumber +=1;
+          numberOfCardsInSuit +=1;
+
+          } // number lt 10
+
+          while ( numberOfCardsInSuit >= 10 && numberOfCardsInSuit <= 12 ) {
+
+            if (numberOfCardsInSuit === 13) {
+              break;
+            }
+
+              let deck = {            
+                  val: 10,   
+                  displayVal: faceCards[faceCardNumber],   
+                  suit: suit,   
+                    } 
+
+             completeDeck.push(deck);
+
+             faceCardNumber +=1;
+             numberOfCardsInSuit +=1;       
+
+          } //number between 10 and 12
+
+          while ( numberOfCardsInSuit == 13 ) {
+
+              let deck = {            
+                 val: 11,   
+                 displayVal: faceCards[faceCardNumber],   
+                 suit: suit,   
+                 } 
+
+                completeDeck.push(deck);
+
+                numberOfCardsInSuit +=1;   
+
+           } //while 13
+
+        } //for loop
+
+        return completeDeck;
+        
 
 // CHECKS
 const deck = getDeck();
