87dd2b3dbe73ddfd44a3a6622523875dc436c09f
Jeffrey Dunaway <jeffreydunaway@users.noreply.github.com>
2022-11-17 20:21:44 -0800

    Update foodIsCooked.js

--- foodIsCooked.js
+++ foodIsCooked.js
@@ -5,69 +5,43 @@
  * @param {string} doneness
  * @returns {boolean} isCooked
  */
-// const foodIsCooked = function(kind, internalTemp, doneness) {
-
-const foodIsCooked = (chicken) => {
-  let temp; 
-  switch (key) {
-    case value:
-      
-      break;
-  
-    default:
-      break;
+const foodIsCooked = function(kind, internalTemp, doneness , isCooked=true) {
+  if(kind =='chicken'){
+    if(internalTemp>165){
+      return `\nChiken is Cooked: ${isCooked}`
+    }else{
+      return `\nChicken is Cooked: ${!isCooked}`
+    }
   }
+  if(kind==='beef'){
+    if(doneness==='rare'){
+      if(125<=internalTemp && internalTemp<135){
+
+        return `\nBeef is Cooked: ${isCooked}`
+      }else{
+        return `\nBeef is Cooked: ${!isCooked}`
+      }
+    }
+    if(doneness==='medium'){
+      if(135<=internalTemp && internalTemp<155){
+        return `\nBeef is Cooked: ${isCooked}`
+      }else{
+        return `\nBeef is Cooked: ${!isCooked}`
+      }
+    }
+    if(doneness==='well'){
+      if(155<=internalTemp && internalTemp<160){
+        return `\nBeef is Cooked: ${isCooked}`
+      }else{
+        return `\nBeef is Cooked: ${!isCooked}`
+      }
+    }
   }
-
-
-// return kind === "chicken" ? true : false
-
-// if (kind =  ("chicken") && internalTemp >= 165 ) then return doneness = "ready to eat."
-
-// return internalTemp === 
-// }
-
-// const foodIsCooked = [foodArray] => {
-//   return foodArray.length >= 1? true : false; 
-// }
-// function foodisCooked (temperature) {
-//   let temp = temperature <= 135 ? "Rare"
-//     :temperature >= 135 && <= 155 ? "Medium"
-//     : temperature >= 155 ? "Welldone
-//     "
-// }
-
-//   if (kind === "chicken") {
-//     if (internalTemp >= 165) {
-//       return true
-//     } else {
-//       return false
-//     }
-//   } else if (kind === "beef") {
-//     if (doneness === "rare") {
-//     return (internalTemp >= 125 && internalTemp < 135)
-//     }
-//     else if (doneness === "medium") {
-//       {
-//         return (internalTemp > 135 && internalTemp <155)
-//       }
-//     }
-//     else if (doneness === "well")
-//       {
-//         return (internalTemp > 155)
-//       }
-//   } else {
-
-//   }
-
-// }
-
-
-
-
+}
 // Test function
 console.log(foodIsCooked('chicken', 90)); // should be false
 console.log(foodIsCooked('chicken', 190)); // should be true
 console.log(foodIsCooked('beef', 138, 'well')); // should be false
 console.log(foodIsCooked('beef', 138, 'medium')); // should be true
-console.log(foodIsCooked('beef', 138, 'rare')); // should be true
+
+console.log(foodIsCooked('beef', 125, 'rare')); // should be true
